87477a1a05c1770aa95189d7a7bfc6e48352cf3d
Coded Journey <coded.journey@gmail.com>
2020-11-11 14:16:08 -0800

    Finished widgets app

---
+++ README.md
@@ -0,0 +1,14 @@
+### React Simple Widgets App
+
+#### A simple react app to get a good understanding of hooks by making widgets
+
+###### What I've Learned:
+
+1. Hooks I've Learned:
+   1. useState
+   1. useEffect
+   1. useRef
+1. Learn basic knowledge of XSS attacks
+1. Events
+1. How to use Google Translate API
+1. Navigation throughout React App
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
+
+// Hard code items
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+];
+
+// Hard coded values for dropdown
+const options = [
+  {
+    label: 'Red',
+    value: 'red'
+  },
+  {
+    label: 'Green',
+    value: 'green'
+  },
+  {
+    label: 'Blue',
+    value: 'blue'
+  }
+];
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a Color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
